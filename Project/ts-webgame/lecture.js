2ca95f5d2842c36fb0fbbbda95da48916479d3b8
Kyun Heo <kyun2dot@gmail.com>
2020-12-03 21:31:27 +0900

    feat: ts 연습

---
+++ Project/ts-webgame/lecture.js
@@ -0,0 +1,31 @@
+var numberOne = Math.ceil(Math.random() * 9);
+var numberTwo = Math.ceil(Math.random() * 9);
+var result = numberOne * numberTwo;
+var word = document.createElement('div');
+word.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
+document.body.append(word);
+var form = document.createElement('form');
+document.body.append(form);
+var input = document.createElement('input');
+input.type = 'number';
+form.append(input);
+var button = document.createElement('div');
+button.textContent = "입력";
+form.append(button);
+var resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (result === Number(input.value)) {
+        resultDiv.textContent = '딩동댕';
+        numberOne = Math.ceil(Math.random() * 9);
+        numberTwo = Math.ceil(Math.random() * 9);
+        result = numberOne * numberTwo;
+        word.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo;
+    }
+    else {
+        resultDiv.textContent = '땡';
+        input.value = "";
+        input.focus();
+    }
+});
